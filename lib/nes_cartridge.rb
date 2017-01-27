79a0137e745646ce8027778ccd979b25470f73da
Jotony Guerra <jotonyguerra@gmail.com>
2017-01-26 18:56:24 -0800

    workwork

--- lib/nes_cartridge.rb
+++ lib/nes_cartridge.rb
@@ -1,9 +1,12 @@
 class NesCartridge
   attr_reader :title, :developer, :year
-  
+
   def initialize(title, developer, year)
     @title = title
     @developer = developer
     @year = year
+  end
+  def to_s
+    asdfasdfasdf 
   end
 end
